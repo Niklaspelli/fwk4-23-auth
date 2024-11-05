e40a5243e9edfda69f9c14febf26269669acc2c9
Niklas Pelli <niklaspelli22@gmail.com>
2024-11-05 10:29:33 +0100

    ratelimit added

--- Dockerfile
+++ Dockerfile
@@ -23,4 +23,4 @@ EXPOSE 3000
 ENV PORT=3000
 
 # Run the app when the container launches
-CMD ["node", "src/service.js"]
+CMD ["node", "src/index.js"]
--- src/service.js
+++ src/service.js
@@ -3,11 +3,23 @@ const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
+const cookierParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+
 const authRoutes = require("./routes/authRoutes.js");
 
 app.use(express.json());
 app.use(helmet());
+app.use(cookierParser());
 app.use(cors());
+
+const requestLimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many request!",
+});
+
+app.use(requestLimit);
 
 app.use("/auth", authRoutes);
 
