8dc5c82669dfcec314c3fcfb8281ebd4ed1197cc
Niklas Pelli <niklaspelli22@gmail.com>
2024-11-08 11:19:00 +0100

    captcha added

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
 const { signJWT, validateJWT } = require("../utils/jwtUtils.js");
 const { hashPassword, verifyPassword } = require("../utils/bcryptjs.js");
+const axios = require("axios");
+
+const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET;
 const pool = require("../utils/connectDB.js");
 
 const registerUser = async (req, res) => {
@@ -14,7 +18,7 @@ const registerUser = async (req, res) => {
   try {
     const [existingUser] = await pool.query(
       "SELECT email FROM users WHERE email = ?",
-      [email],
+      [email]
     );
     if (existingUser.length > 0) {
       return res.status(400).json({ error: "Email already in use" });
@@ -24,7 +28,7 @@ const registerUser = async (req, res) => {
 
     await pool.query(
       "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)",
-      [fullname, email, hashedPassword],
+      [fullname, email, hashedPassword]
     );
     return res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -33,26 +37,50 @@ const registerUser = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, recaptchaToken } = req.body;
+  console.log("RECAPTCHA_SECRET:", process.env.RECAPTCHA_SECRET);
+
+  // Verify the reCAPTCHA token with Google
+  try {
+    const recaptchaResponse = await axios.post(
+      `https://www.google.com/recaptcha/api/siteverify`,
+      null,
+      {
+        params: {
+          secret: RECAPTCHA_SECRET,
+          response: recaptchaToken,
+        },
+      }
+    );
+
+    const { success, "error-codes": errorCodes } = recaptchaResponse.data;
+    if (!success) {
+      console.error("reCAPTCHA verification failed:", errorCodes);
+      return res.status(400).json({ message: "reCAPTCHA verification failed" });
+    }
+  } catch (error) {
+    console.error("Error verifying reCAPTCHA:", error);
+    return res.status(500).json({ message: "Error verifying reCAPTCHA" });
+  }
 
   if (!email || !password) {
     return res.status(400).json({ error: "Email or password value missing" });
   }
+
   try {
     const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
       email,
     ]);
 
     if (rows.length === 0) {
-      return res.status(400).json({ error: "Invalid email or password" });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
 
     const user = rows[0];
 
     const isPasswordValid = await verifyPassword(password, user.password);
-
     if (!isPasswordValid) {
-      return res.status(400).json({ error: "Invalid email or password" });
+      return res.status(400).json({ error: "Invalid credentials" });
     }
 
     const payload = {
@@ -64,6 +92,7 @@ const loginUser = async (req, res) => {
     const token = await signJWT(payload);
     return res.status(200).json({ message: "Login successful", token });
   } catch (error) {
+    console.error("Error during login:", error);
     return res.status(500).json({ error: "Internal server error" });
   }
 };
